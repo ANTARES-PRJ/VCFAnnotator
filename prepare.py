b91f46a1955b1b695721a15b42c2e4a9507c8e09
fassolari1 <fabioassolari1@gmail.com>
2024-04-30 21:45:34 +0200

    Prepare | ✨
    sostitute "<DEL>" with "." using Python

--- prepare.py
+++ prepare.py
@@ -1,8 +1,19 @@
-def prepare_vcf():
-    # Prepare HGMD
-    with open('/path/to/your/vcf/file.vcf', 'r') as file:
+import yaml
+
+def load_config(config_path="config.yaml"):
+    with open(config_path, "r", encoding="utf-8") as file:
+        config = yaml.safe_load(file)
+    return config
+conf = load_config()
+
+
+
+# Prepare HGMD
+for p in conf['prepare']:
+    with open(p['path'], 'r', encoding='utf-8') as file:
+        nome = p['path'].split('/')[-1].split('.')[0]
         # Open a new file for writing the modified VCF data
-        with open('/path/to/your/output/file.vcf', 'w') as output_file:
+        with open(f'result/{nome}.txt', 'w', encoding='utf-8') as output_file:
             for line in file:
                 if line.startswith('#'):
                     output_file.write(line)
@@ -16,7 +27,7 @@ def prepare_vcf():
                 
                 output_file.write('\t'.join(columns))
 
-    print("String replacement complete!")
+print("String \"<DEL>\" replacement complete!")
 
-    # Convert Clinvar DB from VCF to TXT
-    #TODO: ...
+# Convert Clinvar DB from VCF to TXT
+#TODO: ...
